f825e584a557b1c0165bc54cead7f799b7df9eb0
Batuhan Icoz <i@batu.me>
2011-09-01 01:42:20 +0300

    cleanup&added twig

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -5,13 +5,11 @@ require_once __DIR__.'/../vendor/silex.phar';
 use Silex\Application;
 use Silex\Extension\TwigExtension;
 
-use Dailex\Extension\DoctrineMongoExtension;
-
 $app = new Application();
 
 // Register Dailex classes for autoloading
 $app['autoloader']->registerNamespaces(array(
-  'Dailex' => __DIR__,
+  //'BiCom' => __DIR__,
 ));
 
 // Register Silex Extensions
@@ -22,17 +20,6 @@ $app->register(new TwigExtension(), array(
     'debug' => true,
     'cache' => __DIR__.'/../cache',
   ),
-));
-
-$app->register(new DoctrineMongoExtension(), array(
-  'mongodb.odm.document_dir'            => __DIR__.'/Dailex/Document',
-  'mongodb.odm.options'                 => array(
-    'proxy_dir'    => __DIR__.'/../cache',
-    'hydrator_dir' => __DIR__.'/../cache',
-  ),
-  'mongodb.odm.class_path'              => __DIR__.'/../vendor/doctrine-mongodb-odm/lib',
-  'mongodb.doctrine.common.class_path'  => __DIR__.'/../vendor/doctrine-mongodb-odm/lib/vendor/doctrine-common/lib',
-  'mongodb.doctrine.mongodb.class_path' => __DIR__.'/../vendor/doctrine-mongodb-odm/lib/vendor/doctrine-mongodb/lib',
 ));
 
 return $app;
---
+++ vendor/Twig/test/Twig/Tests/ParserTest.php
@@ -0,0 +1,83 @@
+<?php
+
+/*
+ * This file is part of Twig.
+ *
+ * (c) Fabien Potencier
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+class Twig_Tests_ParserTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @expectedException Twig_Error_Syntax
+     */
+    public function testSetMacroThrowsExceptionOnReservedMethods()
+    {
+        $parser = new Twig_Parser(new Twig_Environment());
+        $parser->setMacro('display', $this->getMock('Twig_Node_Macro', null, array(), '', null));
+    }
+
+    /**
+     * @dataProvider getFilterBodyNodesData
+     */
+    public function testFilterBodyNodes($input, $expected)
+    {
+        $parser = $this->getParserForFilterBodyNodes();
+
+        $this->assertEquals($expected, $parser->filterBodyNodes($input));
+    }
+
+    public function getFilterBodyNodesData()
+    {
+        return array(
+            array(
+                new Twig_Node(array(new Twig_Node_Text('   ', 0))),
+                new Twig_Node(array()),
+            ),
+            array(
+                $input = new Twig_Node(array(new Twig_Node_Set(false, new Twig_Node(), new Twig_Node(), 0))),
+                $input,
+            ),
+        );
+    }
+
+    /**
+     * @dataProvider getFilterBodyNodesDataThrowsException
+     * @expectedException Twig_Error_Syntax
+     */
+    public function testFilterBodyNodesThrowsException($input)
+    {
+        $parser = $this->getParserForFilterBodyNodes();
+
+        $parser->filterBodyNodes($input);
+    }
+
+    public function getFilterBodyNodesDataThrowsException()
+    {
+        return array(
+            array(new Twig_Node_Text('foo', 0)),
+            array(new Twig_Node(array(new Twig_Node(array(new Twig_Node_Text('foo', 0)))))),
+        );
+    }
+
+    protected function getParserForFilterBodyNodes()
+    {
+        $parser = new TestParser(new Twig_Environment());
+        $parser->setParent(new Twig_Node());
+        $parser->stream = $this->getMockBuilder('Twig_TokenStream')->disableOriginalConstructor()->getMock();
+
+        return $parser;
+    }
+}
+
+class TestParser extends Twig_Parser
+{
+    public $stream;
+
+    public function filterBodyNodes(Twig_NodeInterface $node)
+    {
+        return parent::filterBodyNodes($node);
+    }
+}
