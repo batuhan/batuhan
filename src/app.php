4b038d8ed79769352f722e99cb4645208ccc4146
Batuhan Icoz <i@batu.me>
2011-09-01 03:11:25 +0300

    routes updates

--- src/app.php
+++ src/app.php
@@ -2,6 +2,20 @@
 
 $app->get('/', function() use ($app) {
   
-  return $app['twig']->render('index.html.twig');
+  return $app['twig']->render('homepage.html.twig');
+  
+});
+
+
+$app->get('/contact', function() use ($app) {
+  
+  return $app['twig']->render('contact.html.twig');
+  
+});
+
+
+$app->get('/about', function() use ($app) {
+  
+  return $app['twig']->render('about.html.twig');
   
 });
