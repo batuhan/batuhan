a0271db12d3e96da322a469e66b71ca89701c7a7
Batuhan Icoz <batuhanicoz@gmail.com>
2011-09-04 02:37:56 +0300

    several changes

--- file_i_may_use/1.php
+++ file_i_may_use/1.php
@@ -1,9 +1,28 @@
 <?php
 
 $browser_lang = explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']);
+$browser_lang = $browser_lang[0];
 
-if($browser_lang[0] === 'tr-TR'){
-	header('Location: http://batuhanicoz.com.tr');
-}else{
-	header('Location: http://batuhanicoz.com');
+if( ! isset($_GET['to']) ){
+	if($browser_lang === 'tr-TR'){
+		header('Location: http://batuhanicoz.com.tr');
+	}else{
+		header('Location: http://batuhanicoz.com');
+	}	
+}else{ $to = $_GET['to'];
+
+	if($to === 'contact'){
+		if($browser_lang === 'tr-TR'){
+			header('Location: http://batuhanicoz.com.tr/iletisim');	
+		} else {
+			header('Location: http://batuhanicoz.com/contact');
+		}
+	}
+	if($to === 'about'){
+		if($browser_lang === 'tr-TR'){
+			header('Location: http://batuhanicoz.com.tr/hakkimda');	
+		} else {
+			header('Location: http://batuhanicoz.com/about');
+		}
+	}
 }
