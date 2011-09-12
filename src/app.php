28a9165c2f25e7da437fe811551dd8fa0e74f81b
Batuhan Icoz <batuhanicoz@gmail.com>
2011-09-12 13:09:34 +0300

    gtalk status icon thingy

--- src/app.php
+++ src/app.php
@@ -40,3 +40,9 @@ $app->get($app['translator']->trans('route_pages').'/{page}', function($page) us
   return $app['twig']->render('pages/'.$page.'.html.twig');
   
 });
+
+$app->get('stuff/gtalk_status_icon', function() use ($app) {
+  
+  header("Location: http://www.google.com/talk/service/badge/Show?tk=z01q6amlqrb27u3ung888bsmshhbl49thtam2dm240eeeha0ks5o4fs185agpmrkd4oi37jetm9pqgtr8bmil6su415251spt8a86v7q65ut5d7rp3osl0lvvhkab9rdl2joretrr8udro2b3qo7bm22he0mae8q6st1n49m0&amp;w=9&amp;h=9");
+  
+});
