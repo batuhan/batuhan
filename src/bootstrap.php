094d2a87eaedbbfe4dfd69d6ea542b0c46cd72b9
Batuhan Icoz <batuhanicoz@gmail.com>
2011-10-12 18:33:46 +0300

    few improvements and upgrades

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,20 +1,22 @@
 <?php
+// some settings
+date_default_timezone_set('Europe/Istanbul');
 
 require_once __DIR__.'/../vendor/silex.phar';
 
 use Silex\Application;
-use Silex\Extension\TwigExtension;
+use Silex\Provider\TwigServiceProvider;
 use Symfony\Component\HttpFoundation\Response;
 
-$app = new Application();
+$app = new Silex\Application();
 
 $app['autoloader']->registerNamespace('Symfony', __DIR__.'/../vendor/symfony/src');
 
-if($_SERVER['REMOTE_ADDR'] === '62.248.41.112')
+if($_SERVER['REMOTE_ADDR'] === '62.248.41.112' OR $_SERVER['SERVER_NAME'] === 'localhost')
 	$app['debug'] = TRUE;
 
-// Register Silex Extensions
-$app->register(new TwigExtension(), array(
+// Register Silex ServiceProviders (aka Extensions)
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
   'twig.path'       => __DIR__.'/../views',
   'twig.class_path' => __DIR__.'/../vendor/Twig/lib',
   'twig.options'    => array(
@@ -23,17 +25,20 @@ $app->register(new TwigExtension(), array(
   ),
 ));
 
-$app['bi_domain'] = $_SERVER['SERVER_NAME'];
 $browser_lang = explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']);
 $app['bi_browser_lang'] = $browser_lang[0];
 
-if($app['bi_domain'] === 'batuhanicoz.com.tr'){
+if($_SERVER['SERVER_NAME'] === 'batuhanicoz.com.tr'){
 	$locale = 'tr';
+}elseif( isset($_GET['lang']) ){
+	$locale = $_GET['lang'];
 }else{
 	$locale = 'en';
 }
 
-$app->register(new Silex\Extension\TranslationExtension(), array( 
+$app['domain'] = $_SERVER['SERVER_NAME']; // @TODO: There should be another (good) way to get a global variable in Twig.
+
+$app->register(new Silex\Provider\TranslationServiceProvider(), array(
     'translation.class_path' =>  __DIR__.'/../vendor/Symfony/src', 
 	'locale' => $locale,
     'locale_fallback' => 'en', 
@@ -46,14 +51,10 @@ $app['translator.messages'] = array(
 
 $app['translator.loader'] = new Symfony\Component\Translation\Loader\YamlFileLoader(); 
 
-
 $app->before(function () use ($app) {
     if ($locale = $app['request']->get('locale')) {
         $app['locale'] = $locale;
     }
 });
 
-$app->get('/{locale}/{message}/{name}', function ($message, $name) use ($app) {
-    return $app['translator']->trans($message, array('%name%' => $name));
-});
 return $app;
